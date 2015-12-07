87c9d525e61fd235f740c2a333a3bcd191fee1db
Bai Jie <baijie1991@gmail.com>
2015-12-07 14:17:00 +0800

    run trying code when click the FloatingActionButton

--- code/client/android/src/main/groovy/gq/baijie/tryit/grooid/MainActivity.java
+++ code/client/android/src/main/groovy/gq/baijie/tryit/grooid/MainActivity.java
@@ -2,20 +2,32 @@ package gq.baijie.tryit.grooid;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mMainTextView;
+
+    public void doTry() {
+        final TestGroovyClass testGroovyClass = new TestGroovyClass();
+        testGroovyClass.setName("Bai Jie");
+        mMainTextView.append(testGroovyClass.getName() + "\n");
+        mMainTextView.append(testGroovyClass.hello() + "\n");
+        mMainTextView.append("tryMetaProgramming:");
+        mMainTextView.append(testGroovyClass.tryMetaProgramming() + "\n");
+        mMainTextView.append("\n");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mMainTextView = (TextView) findViewById(R.id.main_text);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -23,24 +35,9 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                doTry();
             }
         });
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        TextView output = (TextView) findViewById(R.id.main_text);
-        output.append("\n");
-
-        final TestGroovyClass testGroovyClass = new TestGroovyClass();
-        testGroovyClass.setName("Bai Jie");
-        output.append(testGroovyClass.getName() + "\n");
-        output.append(testGroovyClass.hello() + "\n");
-        output.append("tryMetaProgramming:");
-        output.append(testGroovyClass.tryMetaProgramming() + "\n");
     }
 
     @Override
